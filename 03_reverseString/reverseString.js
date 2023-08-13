b876b56758573223614970e47e1391519b5533af
GyHuba <gy.huba@gmail.com>
2023-08-13 21:11:27 +0200

    js exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let array = word.split("")
+    let stringReverse = "";
+    for(let i = array.length-1; i > -1; i--){
+        stringReverse += array[i]
+    }
+    return stringReverse
 };
 
 // Do not edit below this line
