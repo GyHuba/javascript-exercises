8fff5692d39e5a5357f3d5cc91c1ffed9c8d446d
GyHuba <gy.huba@gmail.com>
2023-08-13 20:46:43 +0200

    js exercise 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    if(times === 0){
+        return "";
+    }
+    else if(times < 0){
+        return "ERROR"
+    }
+    let theString;
+    for (let i = times; i > 0; i--) {
+        if (theString === undefined) {
+            theString = word
+        }
+        else {
+            theString = theString + `${word}`
+        }
+    }
+    return theString
 };
 
 // Do not edit below this line
