75c8743d4496f865194a8434264e99379214d641
GyHuba <gy.huba@gmail.com>
2023-08-14 10:33:30 +0200

    js exercise 05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    let finalSum = numberOne
+    if(numberOne < 0 || numberTwo < 0 || typeof numberOne !== "number" ||typeof numberTwo !== "number"){
+        return "ERROR"
+    }
+    if(numberOne < numberTwo){
+        for(let i = numberOne; i < numberTwo; i++){
+            finalSum = finalSum + i+1
+        }
+    }
+    else if(numberOne > numberTwo){
+        for(let i = numberOne; i > numberTwo; i--){
+            finalSum = finalSum + i-1
+        }
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
