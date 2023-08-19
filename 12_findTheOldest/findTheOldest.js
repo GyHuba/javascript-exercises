c42b09d2e800d68c2d9a9087f43d740c89668b25
GyHuba <gy.huba@gmail.com>
2023-08-19 16:41:34 +0200

    js exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let years = [];
+    people.forEach(peopleData => {
+        if (Object.keys(peopleData).length < 3) {
+            const d = new Date();
+            years.push(d.getFullYear() - peopleData.yearOfBirth)
+        }
+        else{
+            years.push(peopleData.yearOfDeath - peopleData.yearOfBirth)
+        }
+    })
+    return people[years.indexOf(Math.max(...years))]
 };
 
 // Do not edit below this line
