abec456118d573c4acef3e0da5a77ccda4eb56bb
GyHuba <gy.huba@gmail.com>
2023-08-19 15:52:40 +0200

    javascript exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    let result = [1]
+    if (Number(number) < 0 || (/[a-zA-Z]/).test(number)) {
+        return "OOPS"
+    }
+    else if (Number(number) < 3) return 1;
+    else {
+        for (let i = 1; i < Number(number) - 1; i++) {
+            if (result.length < 2) {
+                result.push(result[0])
+            }
+            else {
+                result.push(result[0] + result[1]);
+                result.shift();
+            }
+        }
+        return (result[0] + result[1]);
+    }
 };
 
 // Do not edit below this line
