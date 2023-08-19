05d96154a06683e10aca161c95a19a23df1d4f1e
GyHuba <gy.huba@gmail.com>
2023-08-19 12:34:41 +0200

    calculator exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (numberOne, numberTwo) {
+  return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function (numberOne, numberTwo) {
+  return numberOne - numberTwo;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  !array.forEach((number) => {
+    result = result + number;
+  }
+  )
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 0;
+  !array.forEach((number) => {
+    if (result === 0) {
+      result = number;
+    }
+    else {
+      result = result * number;
+    }
+  }
+  )
+  return result;
 };
 
-const power = function() {
-	
+const power = function (numberOne, numberTwo) {
+  let result = numberOne;
+  for(let i = 0; i < numberTwo -1; i++){
+    result = result * numberOne
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if(number === 0){
+    return 1;
+  }
+  let result = 1;
+  for(let i = 1; i < number+1; i++){
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
